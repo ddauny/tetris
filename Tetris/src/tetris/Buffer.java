86682d14b92675984c89c5cd12f98d89e642ba0b
ddauny <dani24iania@gmail.com>
2021-12-17 13:43:26 +0100

    buffer e pacchetto

--- Tetris/src/tetris/Buffer.java
+++ Tetris/src/tetris/Buffer.java
@@ -5,10 +5,49 @@
  */
 package tetris;
 
+import java.awt.List;
+import java.util.ArrayList;
+
 /**
  *
  * @author iania_daniele
  */
 public class Buffer {
+    private ArrayList<Pacchetto> ricevuti;
+    private ArrayList<Pacchetto> daMandare;
+   
+    public Buffer() {
+        ricevuti = new ArrayList<Pacchetto>();
+        daMandare = new ArrayList<Pacchetto>();
+       
+    }
+    
+    public void addPacchettoRicevuto(Pacchetto p){
+        ricevuti.add(p);
+    }
+    
+    public Pacchetto getNextPacchettoRicevuto(){
+        if(ricevuti.size() > 0){
+            Pacchetto p = ricevuti.get(0);
+            ricevuti.remove(0);   
+            return p;
+        } else {
+            return new Pacchetto();
+        }
+    }
+    
+    public void addPacchettoDaMandare(Pacchetto p){
+        daMandare.add(p);
+    }
+    
+    public Pacchetto getNextPacchettoDaMandare(){
+        if(daMandare.size() > 0){
+            Pacchetto p = daMandare.get(0);
+            daMandare.remove(0);   
+            return p;
+        } else {
+            return new Pacchetto();
+        }
+    }
     
 }
--- Tetris/src/tetris/Pacchetto.java
+++ Tetris/src/tetris/Pacchetto.java
@@ -10,6 +10,41 @@ package tetris;
  * @author iania_daniele
  */
 public class Pacchetto {
-    String mittente, destinatario;
+    int stato, righe, malus;
+    String nome;
+    char tipoConnessione;
     
+    public Pacchetto() {
+        stato = -100;//uso stato per sapere se il pacchetto è valido o no
+        righe = 0;
+        malus = 0;
+        nome = "";
+        tipoConnessione = 0;
+    }
+
+    public Pacchetto(int stato, int righe, int malus) {
+        this.stato = stato;
+        this.righe = righe;
+        this.malus = malus;
+        nome = "";
+        tipoConnessione = 0;
+    }
+    
+    public Pacchetto(String p){
+        String[] v = p.split(";");
+        stato = Integer.parseInt(v[0]);
+        righe = Integer.parseInt(v[1]);
+        malus = Integer.parseInt(v[2]);
+        nome = "";
+        tipoConnessione = 0;
+    }
+    
+    public Pacchetto(String nome, char tipoConnessione){
+        this.nome = nome;
+        this.tipoConnessione = tipoConnessione;
+    }    
+    
+    public String toString(Pacchetto p){
+        return p.stato + ";" + p.righe + ";" + p.malus;
+    }
 }
