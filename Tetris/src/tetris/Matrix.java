6e39bc1e039d326ffc0f58c92e38d4c45c1174a2
galliFrancesco <galifranceosco.2809@gmail.com>
2022-01-11 08:45:05 +0100

    Generazione Matrice

--- Tetris/src/tetris/Matrix.java
+++ Tetris/src/tetris/Matrix.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package tetris;
 
 import java.awt.Color;
@@ -10,7 +5,7 @@ import javax.swing.JPanel;
 
 /**
  * 
- * @author giuli
+ * @author galliFranecso
  * 
  * @brief Matrice di gioco, quella base che va a posizionare gli spazi vuoti
  *  per i successivi tetramini
@@ -19,10 +14,9 @@ public class Matrix extends JPanel {
     
     Board b; 
     
-    public Matrix(JPanel[][] tmp, Board b){
-       
+    public Matrix(JPanel[][] tmp, Board b){    
         this.b = b; 
-        int x = 250;
+        int x = 250; // posizione di partenza dei JPanel
         int y = 250;
         
         
