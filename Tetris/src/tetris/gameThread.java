a41f397fb9fd38951e2eab6d6e32c275ce408f4b
ddauny <dani24iania@gmail.com>
2022-01-12 17:49:21 +0100

    vari check

--- Tetris/src/tetris/gameThread.java
+++ Tetris/src/tetris/gameThread.java
@@ -19,7 +19,7 @@ public class gameThread extends Thread {
     Board b;
     Blocco block = new Blocco();
     int direzione = 0;
-    
+
     // Varie classi della gestione del gioco 
     createPiece cp; // genera il nuovo pezzo
     movement mvt; // muove il pezzo
@@ -40,50 +40,50 @@ public class gameThread extends Thread {
         while (true) {
             //System.out.println("direzionee");
             direzione = b.direzione;
-            System.out.println("direzione: " + direzione);
- 
+            //System.out.println("direzione: " + direzione);
+
             if (mvt.getTouched()) {
                 block = new Blocco();
                 cp.run();
                 block = cp.getBlocco();
                 mvt.setTouched(false);
             }
-            switch (direzione) {
-                case 0://scende
             switch (b.direzione) {  // get della direzione ascoltata da Board
                 case 0:// SCENDE NORMALE
                     mvt.muoviBasso();
                     break;
                 case 1:// DESTRA
+                  //  System.out.println("entra");
                     mvt.muoviDestra();
                     mvt.muoviBasso();
                     break;
                 case 2:// SINISTRA
-                    System.out.println("muovi sinistra");
+                //    System.out.println("muovi sinistra");
                     mvt.muoviSinistra();
                     mvt.muoviBasso();
                     break;
                 case 3: // HARD DROP
-                    mvt.hardDrop(); 
-                    break; 
+                    mvt.hardDrop();
+                    break;
                 case 4: // SOFT DROP
-                    mvt.muoviBasso(); 
                     mvt.muoviBasso();
-                break; 
+                    mvt.muoviBasso();
+                    break;
                 case 5: // Rotazione(X)
                     block.ruotaBlocco();
-                break; 
-                    
+                    break;
+
             }
-            
-            b.direzione = 0; 
+            mvt.chkRigheRisolte();
+            b.direzione = 0;
             try {
                 Thread.sleep(1000);
             } catch (InterruptedException ex) {
                 Logger.getLogger(gameThread.class.getName()).log(Level.SEVERE, null, ex);
             }
-            
+
             //!!CONTROLLO DELLE RIGHE!!//
         }
     }
 }
+
